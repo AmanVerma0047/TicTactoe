e35ff5b797a72cdf877f9ff1d5fd61066dc89f4b
Aman Verma <143317849+AmanVerma0047@users.noreply.github.com>
2023-10-15 11:01:53 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,57 +1,85 @@
 #include<stdio.h>
 
-void print_board(p1row_no,p1col_no,p2row_no,p2col_no){
-//printing board tic tac toe;
-for(int row=1;row<4;row++){
-    for(int col = 1;col<4;col++)
-        {
-        if (row == p1row_no && col == p1col_no){
-            printf("X");
-        }
-        else{
-        printf("_");//
-        }
-// for player one..
-        if (row == p2row_no && col == p2col_no){
-            printf("O");
-        }
-        else{
-        printf("_");//
-        }
-//for player two...
-        if(col == 3){
-            printf(" ");
-            }
-        else
-            {
-            printf("|");
-            }
 
-        }printf("\n");
+char board[3][3] = {
+{' ',' ',' '},
+{' ',' ',' '},
+{' ',' ',' '}
+};
+
+void print_board(int player,int get_row,int get_col){
+
+
+if(player ==1){
+board[get_row][get_col] = 'X';
 }
-//printing done...!!!
+else if(player == 2){
+board[get_row][get_col] = 'O';
+}
+
+
+    for(int i =0; i<3;i++){
+        for(int j =0; j<3;j++){
+            printf("| %c  ",board[i][j]);
+
+        }printf("\n ---  ---  ----\n\n");
+    }
+}
+
+
+void get_input(){
+int row,col,player_no;
+printf("{Player-1} Enter row and col as rowcol\n");
+scanf("%d%d",&row,&col);
+player_no = 1;
+
+print_board(player_no,row,col);
+
+printf("{Player-2} Enter row and col as rowcol\n");
+scanf("%d%d",&row,&col);
+player_no = 2;
+print_board(player_no,row,col);
+check_win();
+}
+
+
+char player(){
+if (board[0][0] == 'x'){
+return 1;}
+else if (board[0][0] == 'o'){
+return 2;
+}
+}
+
+
+void check_win(){
+if (board[0][0] == board[0][1] && board[0][0] == board[0][2]){
+printf("%c player wins!", board[0][0]);}
+else if (board[1][0] == board[1][1] && board[0][0] == board[1][2]){
+printf("%c player wins!", board[1][0]);
+}
+else if (board[2][0] == board[2][1] && board[2][0] == board[2][2]){
+printf("%c player wins!", board[2][0]);
+}
+else if(board[0][0] == board[1][0] && board[1][0] == board[2][0]){
+printf("%c player wins!", board[1][0]);
+}
+else if(board[0][1] == board[1][1] && board[1][1] == board[2][1]){
+printf("%c player wins!", board[0][1]);
+}
+else if(board[0][2] == board[1][2] && board[1][2] == board[2][2]){
+printf("%c player wins!", board[1][0]);
+}
+else if(board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+printf("%c player wins!", board[0][0]);}
+else if(board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+printf("%c player wins!", board[0][2]);}
+
+}
+
 }
 
 void main(){
-int choice=1,p1row_no=4,p1col_no=4,p2row_no=4,p2col_no=4;
-print_board(p1row_no,p1col_no,p2row_no,p2col_no);
-
-do {
-printf("{Player-1}Enter row_no.:");
-scanf("%d",&p1row_no);
-printf("{Player-1}Enter col_no.:");
-scanf("%d",&p1col_no);
-
-print_board(p1row_no,p1col_no,p2row_no,p2col_no);
-
-printf("{Player-2}Enter row_no.:");
-scanf("%d",&p1row_no);
-printf("{Player-2}Enter col_no.:");
-scanf("%d",&p1col_no);
-
-print_board(p1row_no,p1col_no,p2row_no,p2col_no);
-
-
-}while(choice!=0);
+get_input();
 
 }
